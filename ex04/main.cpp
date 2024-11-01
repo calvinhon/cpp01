7a9f0bff6920d3672a91df67207ba1a19a296f00
Calvinhon <calvin.r.hon@gmail.com>
2024-11-01 12:32:45 +0400

    empty strings

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -2,29 +2,41 @@
 #include <fstream>
 #include <string>
 
+void    copy(std::ofstream& outFile, std::string line, std::string s1, std::string s2) {
+    size_t  position = 0;
+    size_t  lastPos = 0;
+    size_t  len = s1.length();
+
+    while ((position = line.find(s1, lastPos)) != std::string::npos) {
+        outFile << line.substr(lastPos, position - lastPos);
+        outFile << s2;
+        lastPos = position + len;
+    }
+    outFile << line.substr(lastPos);
+}
+
 int main(int ac, char** av) {
-    if (ac != 4)
+    if (ac != 4) {
+        std::cerr << "Input 3 arguements: [filename] [string to delete] [string to insert]" << std::endl;
         return 1;
-    std::ifstream inFile(av[0]);
+    }
+    std::ifstream inFile(av[1]);
     if (!inFile) {
         std::cerr << "Error opening input file" << std::endl;
         return 1;
     }
-    std::ofstream outFile(av[0] + ".replace");
+    std::ofstream outFile(std::string(av[1]) + ".replace");
     if (!outFile) {
         std::cerr << "Error opening output file" << std::endl;
         return 1;
     }
-    std::string line;
-    size_t      position;
-    size_t      len = av[1].length();
-    while (std::getline(inFile, line)) {
-        position = line.find(av[1]);
-        if (position == std::string::npos)
-            outFile << line << '\n';
-        else
-            
+    if (std::string(av[2]).empty()) {
+        std::cerr << "Error opening output file" << std::endl;
+        return 1;
     }
+    std::string line;
+    while (std::getline(inFile, line))
+        copy(outFile, line, av[2], av[3]);            
     inFile.close();
     outFile.close();
     return 0;
